5b85847ab15aa24a3611001db514f71b6d1f9244
Dimitar Stanev <jimstanev@gmail.com>
2020-11-25 23:34:49 +0100

    results for muscle optimization

---
+++ data/gait1992/real_time/muscle_optimization/pdfextract.sh
@@ -0,0 +1,9 @@
+pdftk muscle_optimization_comparison.pdf cat 7 output mo_semimem_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 10 output mo_bifemsh_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 22 output mo_glut_max3_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 24 output mo_psoas_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 28 output mo_rect_fem_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 29 output mo_vas_med_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 32 output mo_med_gas_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 34 output mo_soleus_r.pdf
+pdftk muscle_optimization_comparison.pdf cat 38 output mo_tib_ant_r.pdf
